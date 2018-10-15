10654e835cf4930ba525ce6f937309f11d261988
Alexander Carlin <ahcarlin@gmail.com>
2018-10-15 16:52:44 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
   def kind
     sides = [side1, side2, side3]
     sides.sort
-    if ((side1 + side2) < side3)  || ((self.side1 <= 0 || self.side2 <= 0) || self.side3 <= 0) 
+    if ((sides[0] + sides[1]) < sides[2])  || ((self.side1 <= 0 || self.side2 <= 0) || self.side3 <= 0) 
         raise TriangleError.new("Please enter a valid triangle")
     elsif self.side1 == self.side2 && self.side2 == self.side3
       return :equilateral
